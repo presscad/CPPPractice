272dacaa73983851f7ab32fb365636a0c027e287
longlinht <longlinht@gmail.com>
2014-12-03 15:50:46 +0800

    add compare float function;

--- CPP/CPP/interview.h
+++ CPP/CPP/interview.h
@@ -74,4 +74,16 @@ void mergeVector(std::vector<T>& a, const std::vector<T> &b)
 {
 	a.insert(a.end(), b.begin(), b.end());
 }
+
+//Most effective way for float and double comparison
+template<class T>
+typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type
+almost_equal(T x, T y, int ulp)
+{
+	// the machine epsilon has to be scaled to the magnitude of the values used
+	// and multiplied by the desired precision in ULPs (units in the last place)
+	return std::abs(x - y) < std::numeric_limits<T>::epsilon() * std::abs(x + y) * ulp
+		// unless the result is subnormal
+		|| std::abs(x - y) < std::numeric_limits<T>::min();
+}
 #endif // INTERVIEW_H
