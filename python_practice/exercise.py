a865c0136a42794e391c32c4e6aefebfc1b16292
longlinht <longlinht@gmail.com>
2015-01-14 13:51:56 +0800

    modify python practices;

--- python_practice/exercise.py
+++ python_practice/exercise.py
@@ -126,7 +126,147 @@ while b < 10:
 i = 256*256
 print 'The value of i is', i
 
+#if statements
+x = int(raw_input('Please enter an integer: '))
+if x < 0:
+	x = 0
+	print 'Negative changed to zero'
+elif x == 0:
+	print 'Zero'
+elif x == 1:
+	print 'Single'
+else:
+	print 'More'
 
+#for statements
+words = ['cat', 'window', 'defenestrate']
+for w in words:
+	print w, len(w)
+	
+for w in words[:]:  # Loop over a slice copy of the entire list.
+	if len(w) > 6:
+		words.insert(0, w)
+print words
 
+#range() function
+print range(10)
+print range(5, 10)
+print range(0, 10, 3)
+print range(-10, -100, -30)
 
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+	print i, a[i]
+	
+for n in range(2, 10):
+	for x in range(2, n):
+		if n % x == 0:
+			print n, 'equals', x, '*', n/x
+			break
+		else:
+			# loop fell through without finding a factor
+			print n, 'is a prime number'
+			
+#while True:
+#	pass  # Busy-wait for keyboard interrupt (Ctrl+C)
+	
+def fib(n):    # write Fibonacci series up to n
+	"""Print a Fibonacci series up to n."""
+	a, b = 0, 1
+	while a < n:
+		print a,
+		a, b = b, a+b
+# Now call the function we just defined:
+fib(2000)
 
+def ask_ok(prompt, retries=4, complaint='Yes or no, please!'):
+    while True:
+        ok = raw_input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise IOError('refusenik user')
+        print complaint
+
+ask_ok('Do you really want to quit?')
+
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print voltage,state,action,type
+	
+parrot(1000)                                          # 1 positional argument
+parrot(voltage=1000)                                  # 1 keyword argument
+parrot(voltage=1000000, action='VOOOOOM')             # 2 keyword arguments
+parrot(action='VOOOOOM', voltage=1000000)             # 2 keyword arguments
+parrot('a million', 'bereft of life', 'jump')         # 3 positional arguments
+parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
+
+def cheeseshop(kind, *arguments, **keywords):
+    print "-- Do you have any", kind, "?"
+    print "-- I'm sorry, we're all out of", kind
+    for arg in arguments:
+        print arg
+    print "-" * 40
+    keys = sorted(keywords.keys())
+    for kw in keys:
+        print kw, ":", keywords[kw]
+		
+cheeseshop("Limburger", "It's very runny, sir.",
+           "It's really very, VERY runny, sir.",
+           shopkeeper='Michael Palin',
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")
+		   
+range(3, 6)             # normal call with separate arguments
+args = [3, 6]
+print range(*args)            # call with arguments unpacked from a list
+
+def make_incrementor(n):
+	return lambda x: x + n
+	
+f = make_incrementor(42)
+print f(0)
+print f(1)
+
+pairs = [(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four')]
+pairs.sort(key=lambda pair: pair[1])
+print pairs
+
+def my_function():
+	"""Do nothing, but document it.
+	
+	No, really, it doesn't do anything.
+	"""
+	pass
+	
+print my_function.__doc__
+
+#Coding Style:
+#Use 4-space indentation, and no tabs.
+#4 spaces are a good compromise between small indentation (allows greater nesting depth) and large indentation (easier to read). Tabs introduce confusion, and are best left out.
+#Wrap lines so that they don’t exceed 79 characters.
+#This helps users with small displays and makes it possible to have several code files side-by-side on larger displays.
+#Use blank lines to separate functions and classes, and larger blocks of code inside functions.
+#When possible, put comments on a line of their own.
+#Use docstrings.
+#Use spaces around operators and after commas, but not directly inside bracketing constructs: a = f(1, 2) + g(3, 4).
+#Name your classes and functions consistently; the convention is to use CamelCase for classes and lower_case_with_underscores for functions and methods. 
+#Always use self as the name for the first method argument (see A First Look at Classes for more on classes and methods).
+#Don’t use fancy encodings if your code is meant to be used in international environments. Plain ASCII works best in any case.
+
+a = [66.25, 333, 333, 1, 1234.5]
+print a.count(333), a.count(66.25), a.count('x')
+a.insert(2, -1)
+a.append(333)
+print a
+print a.index(333)
+a.remove(333)
+print a
+a.reverse()
+print a
+a.sort()
+print a
+print a.pop()
+print a
