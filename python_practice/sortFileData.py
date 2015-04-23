d832f7aae03700c7069abb83632dd36a9d6f32d7
longlinht <hetaoof@yahoo.com>
2015-04-23 16:06:10 +0800

    modify;

--- python_practice/sortFileData.py
+++ python_practice/sortFileData.py
@@ -5,39 +5,37 @@ parsedData = open("parsedData.txt","w")
 speedPairs = {}
 for line in cSpeedData:
     lineStr = line.split(";")
-    if len(lineStr) != 2:
+    print lineStr
+    if len(lineStr) != 3:
         continue
     elif len(lineStr[0]) == 0 or len(lineStr[1]) == 0:
         continue
-    elif lineStr[1] == "-1B/s\n" :
+    elif lineStr[2] == "-1B/s\n" :
         continue
     else:
-        pos = lineStr[1].find("B")
-        speed = lineStr[1][:pos]
-        speedValue = int(speed)
-        if int(speedValue) > 1024:
-            speedValue /= 1024
-            speedStr = lineStr[0] + ";" + str(speedValue) + "KB/s\n"
-        else:
-            temp = speedValue
-            speedValue /= 1024
-            bValue = temp % 1024
-            speedStr = lineStr[0] + ";" + str(bValue) + "KB/s\n"
-        #parsedData.write(speedStr)
-        speedPairs[lineStr[0]] = int(speed)
+        pos = lineStr[2].find("B")
+        speed = lineStr[2][:pos]
+        speedPairs[lineStr[0]+lineStr[1]] = int(speed)
 
-print "Before...\n"
-print speedPairs
-sorted(speedPairs.iteritems(), key=lambda t:t[1])
-print "After...\n"
-print speedPairs
+from operator import itemgetter
+sortedPairs = sorted(speedPairs.items(), key=itemgetter(1))
 
-for k in speedPairs:
-    parsedData.write(k)
+for k in sortedPairs:
+    parsedData.write(k[0])
     parsedData.write(";")
-    parsedData.write(str(speedPairs[k]))
-    parsedData.write("KB/s\n")
+    value = k[1]
+    speed = ""
+    if value > (1024*1024):
+        value /= (1024*1024)
+        speed = str(value) + "MB/s\n"
+    elif value > 1024:
+        value /= 1024
+        speed = str(value) + "KB/s\n"
+    else:
+        value %= 1024
+        speed = str(value) + "B/s\n"
 
+    parsedData.write(speed)
 cSpeedData.close()
 parsedData.flush()
 parsedData.close()
